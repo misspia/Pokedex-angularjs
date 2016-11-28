4b3f7ffd9c9c1349eb28d8a6341be059293d442d
misspia <pia_leung@hotmail.com>
2016-11-27 19:11:12 -0500

    gulpfile init

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,29 @@
 // https://css-tricks.com/gulp-for-beginners/
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  gulp.watch('**/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    }
+  })
+})
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/main.scss')
+    .pipe(sass()) 
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
